e438f7b196ceb3458cea4ad221bd5fbd7ddc33b4
Jose Arias <115059715+Monoarias777@users.noreply.github.com>
2023-06-29 12:00:22 -0400

    YC

---
+++ src/Components/BeContacted.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import '../Styles/BeContacted.css'
+
+
+function BeContactedForm() {
+    const { register, handleSubmit } = useForm();
+    const [data, setData] = useState("");
+  
+    return (
+     
+     <div className="formContainer">
+     <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+        <input className="BorderForm" {...register("firstName")} placeholder="First name" />
+        <select className="BorderForm" {...register("category", { required: true })}>
+          <option value="">Choose Project...</option>
+          <option value="A">Houses</option>
+          <option value="B">Buildings-Offices</option>
+          <option value="C">Post-Construction</option>
+        </select>
+        <input className="BorderForm" type="number" {...register("YourNumber")} placeholder="Your Number" />
+        <p>{data}</p>
+        <input type="submit" />
+      </form>
+      </div>
+    );
+  }
+
+  export default BeContactedForm;
--- src/Components/whatWeDo.js
+++ src/Components/whatWeDo.js
@@ -1,13 +1,16 @@
 import React from "react";
+import '../Styles/whatWeDo.css'
+import BeContactedForm from "./BeContacted";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function WhatWeDo(){
 return (
 
 
-<div className="WhatWeDoContainer">
+<div className="WhatWeDoContainer row">
 
-<div className="WhatWeDoLeft">
-<p>
+<div className="WhatWeDoLeft col-md-6">
+<p id="CompanyDescription">
 We are a professional cleaning company based in Toronto, providing top-notch cleaning services for residential homes, commercial offices and post-construction sites
 </p>
 
@@ -36,11 +39,34 @@ alt="House Background"
 />
 </div>
 
-<div className="WhatWeDoRight">
-  
+<div className="WhatWeDoRight col-md-6">
+
+<img 
+className="CleaningImg"
+src={require('../Images/Cleaning Supply.jpg')}
+alt="House Background" 
+/>
+
+<div className="SocialIcons">
+<a href="URL" target="_blank" rel="noopener noreferrer">
+<img alt="siguenos en instagram" height="60" width="60" src={require('../Images/instagram.png')} title="siguenos en instagram" /></a>
+
+<a href="https://wa.me/16474252986" target="_blank" rel="noopener noreferrer">
+<img alt="siguenos en whatsapp" height="60" width="60" src={require('../Images/whatsapp.png')} title="siguenos en whatsapp" /></a>
+
+<a href="mailto:gojosearias@gmail.com" target="_blank" rel="noopener noreferrer">
+<img alt="Email Us" height="60" width="60" src={require('../Images/mail.png')} title="siguenos en whatsapp" /></a>
+
 </div>
 
-
+<a href="tel:+16474252986">647-425-2986</a>
+<h1 className="h1">
+        <img id="phone" src={require('../Images/icons phone.png')}
+        alt='Phone Icone' />
+       Drop your Number to be Contacted!
+      </h1>
+      <BeContactedForm />
+</div>
 
 </div>
 
