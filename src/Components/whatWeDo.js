a718436390503c54151e7561b5b6528d565db93a
Jose Arias <115059715+JoseArias31@users.noreply.github.com>
2023-08-29 17:39:30 -0400

    Service Updates

--- src/Components/whatWeDo.js
+++ src/Components/whatWeDo.js
@@ -1,21 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import '../Styles/whatWeDo.css'
 import BeContactedForm from "./BeContacted";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ImgHover from "./CleaningImg";
 
 
 function WhatWeDo(){
+      const [showList, setShowList] = useState(true);
+
+      const toggleList = () => {
+        setShowList(!showList);
+      };
 return (
 
 
 <div className="WhatWeDoContainer row">
+<div className="WhatWeDoLeft col-md-6"></div>
+<div className="col-sm-6 diagonal Colm1 listFS">
+  <div className="WhatWeDoLeft col-md-6">
+  <h1 className="TextFS">Flexible Services</h1>
+<p id="CompanyDescription">
+We are a professional cleaning company based in Toronto, providing top-notch cleaning services for residential homes, commercial offices and post-construction sites
+</p>
 
-
-<div className="WhatWeDoRight col-md-6">
-<div className="ContainerCleaningImg
-">
+<div className="WhatWeDoLeftbottom">
+<ImgHover />
 
 </div>
+</div>
+
+    
+<div className="containerList">
+      <div className="row">
+        <div className="col-md-6">
+          {showList ? (
+            <ul className="text-lg text-md-35 FlexibleServicesList live">
+              <li className="listFS circle-checkmark">Office Spaces</li>
+              <li className="listFS circle-checkmark">Conference Room</li>
+              <li className="listFS circle-checkmark">Workstations</li>
+              <li className="listFS circle-checkmark">Copy/printing room</li>
+              <li className="listFS circle-checkmark">Floors</li>
+              <li className="listFS circle-checkmark">Restrooms</li>
+            </ul>
+          ) : (
+            <button className="btn btn-primary" onClick={toggleList}>
+              Show List
+            </button>
+          )}
+        </div>
+        <div className="col-md-6">
+          {showList ? (
+            <ul className="text-lg text-md-35 FlexibleServicesList live">
+              <li className="listFS circle-checkmark">Kitchens</li>
+              <li className="listFS circle-checkmark">Hallways</li>
+              <li className="listFS circle-checkmark">Basements</li>
+              <li className="listFS circle-checkmark">Glasses</li>
+              <li className="listFS circle-checkmark">Walls</li>
+              <li className="listFS circle-checkmark">Lobby</li>
+            </ul>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </div>
+    </div>
+  
+
+  </div>
+
+<div className="WhatWeDoRight col-md-6">
+
 {/* <div className="SocialIcons">
 <a href="https://instagram.com/gojosearias?igshid=YmMyMTA2M2Y=" target="_blank" rel="noopener noreferrer">
 <img alt="siguenos en instagram" height="60" width="60" src={require('../Images/instagram.png')} title="siguenos en instagram" /></a>
