b98d29a84a8535fd4036d95f5ac0163d7de7a01d
Jose Arias <115059715+JoseArias31@users.noreply.github.com>
2023-09-07 13:30:30 -0400

    Home nav updated

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -14,7 +14,7 @@ function NavBar() {
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="">Home</a>
+            <a class="nav-link active" aria-current="page" href="#home">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#projects">What We Do</a>
